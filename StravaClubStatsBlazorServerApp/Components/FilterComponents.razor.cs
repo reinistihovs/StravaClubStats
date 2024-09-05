77ee5dcf4ebce3e395de7afb2504f0f986c1781d
John Tomlinson <john.tomlinson@teamsynergi.co.uk>
2024-09-05 15:30:46 +0100

    Added the FilterCompoents component

---
+++ StravaClubStatsBlazorServerApp/Components/FilterComponents.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace StravaClubStatsBlazorServerApp.Components;
+
+public partial class FilterComponents
+{
+    [Parameter]
+    public List<string> Cyclists { get; set; } = null!;
+
+    [Parameter]
+    public string? Cyclist { get; set; }
+
+    [Parameter]
+    public EventCallback<string?> CyclistChanged { get; set; }
+
+    [Parameter]
+    public bool IsSmall { get; set; }
+}
